b16d12f6c334c3c55fecb1021d5a3c6966b021f9
phillipv20 <phillip.vinson@bobcats.gcsu.edu>
2014-03-15 15:43:25 -0400

    Nothing
    Nothing, forced to update for some reason.

---
+++ Lockerly_Arboretum_App/adminLog.js
@@ -0,0 +1,7 @@
+//This is to be called when admin Login button is pressed
+//Will handle the login procedure
+
+function adminLog()
+{
+	$("p").text("You have called the admin login function, congratulations!");
+}
