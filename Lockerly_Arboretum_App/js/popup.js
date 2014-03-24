b9a967f068d8926538eed2243dc70007564c28d6
phillipv20 <phillip.vinson@bobcats.gcsu.edu>
2014-03-24 10:17:36 -0400

    Updated Error Message
    Got font changed to black, and centered properly on p tags

--- Lockerly_Arboretum_App/js/popup.js
+++ Lockerly_Arboretum_App/js/popup.js
@@ -1,16 +1,16 @@
 //pop up error message controller.
 
-var popup = function(msg){
-	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all'><p>"+msg+"</p></div>")
+var popup = function(msg)
+{
+	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all'><p class = 'black'>" +  msg  + "</p></div>")
 	.css({ display: "block", 
 		opacity: 1.00, 
 		position: "fixed",
 		padding: "7px",
 		"text-align": "center",
 		width: "500px",
-		left: "50%",
-		top: "20%",
-		"color": "Red"})	//Not working, want to change color of font. also tried doing this in the p section above
+		left: "35%",
+		top: "20%" })
 	.appendTo( $.mobile.pageContainer ).delay( 1500 )
 	.fadeOut( 400, function(){
 		$(this).remove();
